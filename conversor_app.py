b54a7b65aaababfe696abf8a390aa3d269a8a5ea
Isabel de Sousa <72025926+icn-sousa@users.noreply.github.com>
2023-09-16 16:37:57 -0300

    Add files via upload

---
+++ conversor_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.write("""
+# Simples Conversor de Área 
+
+Converte quilômetros quadrados para hectares
+
+""")
+
+# INPUT: área em km²
+area_km2 = st.number_input("Área (km²):")
+
+# Conversão
+area_ha = area_km2 * 100
+
+# Confere se botão foi pressionado
+if(st.button('Converter')):
+ 
+    # Print área em hectares
+    st.text("Área: {} ha.".format(area_ha))
